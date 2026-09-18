919719c5d679509fb650ee31df530c30e13a68f7
Piyush Velhal <velhalpiyush66@gmail.com>
2026-09-18 16:48:49 +0530

    Update multer storage destination to use path.join

--- server/middlewares/multer.js
+++ server/middlewares/multer.js
@@ -1,8 +1,9 @@
 import multer from "multer";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination : function (req, file, cb){
-        cb(null, "public")
+        cb(null, path.join(process.cwd(), "public"));
     },
     filename: function (req, file, cb){
         const filename =Date.now() + "-" + file.originalname;
